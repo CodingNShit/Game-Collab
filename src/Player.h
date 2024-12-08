7e621867acfcca30415a2610726110fd40f09146
CodingNShit <learningcodingnshit@gmail.com>
2024-12-08 12:14:35 +0800

    Add files via upload

---
+++ src/Player.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SDL2/SDL.h>
+
+class Player {
+public:
+  Player(int x, int y, int width, int height);
+  void handleInput();
+  void render(SDL_Renderer* renderer) const;
+
+private:
+  int x, y;
+  int width, height;
+};
+
+#endif // PLAYER_H
+
